a3ab1f64ee2e51b76c3d62318cf659eea29511dd
Danny Pifer <piferdg@gmail.com>
2018-10-01 10:34:24 -0600

    Adding back-end api for data use, also refactored the code a bit to work with the new API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Here what Im doing is creating elements by using 'document.createElement' and Im also grabbing elements by using 'document.getElement'. 
 //This will allow me to use these elements to populate the data (family members). It will also allow me to use the button. 
-let family = document.getElementsByClassName('family-members')
+let myFamily = document.getElementsByClassName('family-members')
 let giftGivers = document.getElementsByClassName('random-group')[0]
 let personOne = document.createElement('li')
 let personTwo = document.createElement('li')
@@ -13,11 +13,11 @@ let personEight = document.createElement('li')
 let familyNames = document.getElementById('names')
 let randomButton = document.getElementById('random-button')
 
-//Here I'm importing the local json file that Im using to store my data. 
-let apiURL = "./family.json"
+//Here I'm setting a variable store my back-end data API URL. I was using a local json (family.json) but then decided to make a back-end and deploy it ot heroku. 
+let apiURL = "https://secret-santa-challenge.herokuapp.com/family"
 
 //This function is fetching the family member data, then I'm using a for loop to iterate through the data in order to return a randomized list. 
-//By using.sort, and Math.random Im able to return a randomized array of family members. 
+//By using .sort, and Math.random I'm able to return a randomized array of family members. 
 //In line 25 I'm using console.log to check that Im getting the data that I want before populating it on the page.
 //There is one problem with this solution in its current form, sometimes family members names will match. Meaning sometimes 'Danny' will get 'Danny' as a secret santa. This can be resolved by pressing the 'Randomize' button again.
 //I would have to make an if/else statement to make sure that the the secret santa is '=== !Danny' (I'd need to do this for each name)  
@@ -25,20 +25,21 @@ function getData() {
   let giftGiving = []
   fetch (apiURL)
   .then (response => response.json())
-  .then (familyMembers = familyMembers => {
-    for (i=0; i<familyMembers.length; i++) {
-      giftGiving.push(familyMembers[i].name)
+  .then (familyMembers = familyMembers => 
+    {
+    for (i=0; i<familyMembers.family.length; i++) {
+      giftGiving.push(familyMembers.family[i])
       giftGiving.sort(() => Math.random() -0.5) 
     }
-    console.log('New', giftGiving)
-    personOne.innerText = giftGiving[0]
-    personTwo.innerText = giftGiving[1]
-    personThree.innerText = giftGiving[2]
-    personFour.innerText = giftGiving[3]
-    personFive.innerText = giftGiving[4]
-    personSix.innerText = giftGiving[5]
-    personSeven.innerText = giftGiving[6]
-    personEight.innerText = giftGiving[7]
+    console.log('Secret Santas', giftGiving)
+    personOne.innerText = giftGiving[0].name
+    personTwo.innerText = giftGiving[1].name
+    personThree.innerText = giftGiving[2].name
+    personFour.innerText = giftGiving[3].name
+    personFive.innerText = giftGiving[4].name
+    personSix.innerText = giftGiving[5].name
+    personSeven.innerText = giftGiving[6].name
+    personEight.innerText = giftGiving[7].name
   })
   
 }
